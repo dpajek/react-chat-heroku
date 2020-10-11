d6772acfe48173396ec549428fb6f8e85561c48d
dpajek <dan.pajek@gmail.com>
2020-10-11 14:08:46 -0400

    test without * endpoint pointing to index.html

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const socketio = require("socket.io");
 const io = socketio(http);
 
 // Serve static React app at route directory of server
-// app.use is the middleware and applies to all, independent of call
-// points to this folder for static files
-
-// I think this is necessary if other files are needed (maybe images? css?)
-//app.use(express.static(path.join(__dirname, "client/build")));
+// app.use is the middleware and applies to all, independent of call method
+// The React app won't function properly without this
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // list for connection event
 io.on("connection", (socket) => {
@@ -36,9 +34,11 @@ app.get("/hello-world", (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 // I think this is needed on server to serve React file
+/*
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
+*/
 
 const port = process.env.PORT || 5000;
 http.listen(port);
