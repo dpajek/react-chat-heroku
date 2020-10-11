1ac62cb9dbfd09287c6276059aa2d2b669767b8b
dpajek <dan.pajek@gmail.com>
2020-10-11 00:23:31 -0400

    sockets seems to work locally...

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
+import "./App.css";
+
+const ENDPOINT = "/";
+let socket;
 
 function App() {
+  const [chats, setChats] = useState("");
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect called to setup");
+    socket = socketIOClient(ENDPOINT);
+
+    return () => socket.disconnect();
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect called to receive message");
+
+    socket.on("chat message", (data) => {
+      console.log("chat from server: " + data);
+      //console.log(chats);
+      setChats((chats) => chats + " " + data);
+    });
+  }, []);
+
+  function newChatMessage(e) {
+    e.preventDefault();
+
+    console.log("newChatMessage: " + currentMessage);
+    socket.emit("chat message", currentMessage);
+    setCurrentMessage("");
+  }
+
+  function onChangeCurrentMessage(e) {
+    setCurrentMessage(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {chats}
+      <div className="form">
+        <input
+          type="text"
+          className="currentMessage"
+          value={currentMessage}
+          onChange={onChangeCurrentMessage}
+          onKeyPress={e => e.key === 'Enter' ? newChatMessage(e) : null }
+          autoComplete="off"
+        />
+        <button onClick={(e) => newChatMessage(e)}>Send</button>
+      </div>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
+
+const http = require("http").createServer(app);
+
+const socketio = require("socket.io");
+const io = socketio(http);
 
 // Serve static React app at route directory of server
 // app.use is the middleware and applies to all, independent of call
 // points to this folder for static files
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('/hello-world', (req,res) => {
-    res.send('<h1>Hello World!</h1>');
+// list for connection event
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    console.log("server - chat message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+app.get("/hello-world", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
+http.listen(port);
 
 console.log(`React Cloud Chat listening on ${port}`);
